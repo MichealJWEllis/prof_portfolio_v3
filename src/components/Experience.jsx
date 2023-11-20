ed13c442f0a388fcf2dbf0c508a359d578e7be07
mellis <mellis.thedev@gmail.com>
2023-11-20 11:57:52 -0500

    added resume download

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -9,50 +9,53 @@ import { styles } from "../styles";
 import { experiences } from "../constants";
 import { SectionWrapper } from "../hoc";
 import { textVariant } from "../utils/motion";
+import Button from "@mui/material/Button";
 
 const ExperienceCard = ({ experience }) => {
     return (
-        <VerticalTimelineElement
-            contentStyle={{
-                background: "#203047",
-                color: "#fff",
-            }}
-            contentArrowStyle={{ borderRight: "7px solid  #232631" }}
-            date={experience.date}
-            iconStyle={{ background: experience.iconBg }}
-            icon={
-                <div className="flex justify-center items-center w-full h-full">
-                    <img
-                        src={experience.icon}
-                        alt={experience.company_name}
-                        className="w-[60%] h-[60%] object-contain"
-                    />
-                </div>
-            }
-        >
-            <div>
-                <h3 className="text-white text-[24px] font-bold">
-                    {experience.title}
-                </h3>
-                <p
-                    className="text-[grey] text-[16px] font-semibold"
-                    style={{ margin: 0 }}
-                >
-                    {experience.company_name}
-                </p>
-            </div>
-
-            <ul className="mt-5 list-disc ml-5 space-y-2">
-                {experience.points.map((point, index) => (
-                    <li
-                        key={`experience-point-${index}`}
-                        className="text-white-100 text-[14px] pl-1 tracking-wider"
+        <>
+            <VerticalTimelineElement
+                contentStyle={{
+                    background: "#203047",
+                    color: "#fff",
+                }}
+                contentArrowStyle={{ borderRight: "7px solid  #232631" }}
+                date={experience.date}
+                iconStyle={{ background: experience.iconBg }}
+                icon={
+                    <div className="flex justify-center items-center w-full h-full">
+                        <img
+                            src={experience.icon}
+                            alt={experience.company_name}
+                            className="w-[60%] h-[60%] object-contain"
+                        />
+                    </div>
+                }
+            >
+                <div>
+                    <h3 className="text-white text-[24px] font-bold">
+                        {experience.title}
+                    </h3>
+                    <p
+                        className="text-[grey] text-[16px] font-semibold"
+                        style={{ margin: 0 }}
                     >
-                        {point}
-                    </li>
-                ))}
-            </ul>
-        </VerticalTimelineElement>
+                        {experience.company_name}
+                    </p>
+                </div>
+
+                <ul className="mt-5 list-disc ml-5 space-y-2">
+                    {experience.points.map((point, index) => (
+                        <li
+                            key={`experience-point-${index}`}
+                            className="text-white-100 text-[14px] pl-1 tracking-wider"
+                        >
+                            {point}
+                        </li>
+                    ))}
+                </ul>
+            </VerticalTimelineElement>
+        </>
     );
 };
 
@@ -77,6 +80,22 @@ const Experience = () => {
                         />
                     ))}
                 </VerticalTimeline>
+                <a
+                    href="../assets/Micheal JW Ellis Sr_5-2-23_Resume.docx"
+                    download="Resume.pdf"
+                    style={{ textDecoration: "none" }}
+                >
+                    <Button
+                        style={{
+                            marginTop: "20px",
+                            maxWidth: "200px",
+                            color: "white",
+                        }}
+                        variant="outlined"
+                    >
+                        Download Resume
+                    </Button>
+                </a>
             </div>
         </>
     );
